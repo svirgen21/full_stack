49efdba81115fd9ac3c88c96c3136df13695bdc2
gvirgen <gvirgen21@gmail.com>
2025-02-18 19:35:09 -0800

    agregamos nuevos cambios

---
+++ clase_05/formulario.js
@@ -0,0 +1,43 @@
+let nombre;
+let edad;
+
+
+
+
+
+function saludar(){
+    console.log('Hola');
+}
+/*este es un comentario de 
+varios saltos d elinea
+
+Variables en JS
+1. una Variable en JS es un espacio de memoria que se utiliza para almacenar un valor
+ */
+var provincia;
+provincia = "Buenos Aires";// Asignacion de valor ala variable tipo string: texto
+
+var edad = 50;// declaracion y asignacion de valor ala variable tipo number : numero
+
+var pais = 'Argentina';
+
+var habitantes = "5000";
+
+
+var dni = "a2443444";
+
+var iva = "20";
+
+// en java 
+
+//
+let localidad = 'Lomas de Zamora';
+
+let barrio = "Banfield";
+
+const IVA =21; // declaracion y asignaion de valor ala variable constante y que no cambia de tipo number: numero
+
+edad = 25;
+console.log (IVA);
+console.log(edad);
+
